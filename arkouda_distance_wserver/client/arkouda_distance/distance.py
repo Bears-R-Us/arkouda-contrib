ada9fcb156838c23e174ba6460a2570a3e1ab47c
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-03-29 11:06:00 -0400

    Updating structure of python file.

--- arkouda_distance_wserver/client/arkouda_distance/distance.py
+++ arkouda_distance_wserver/client/arkouda_distance/distance.py
@@ -8,26 +8,17 @@ def dot(u: ak.pdarray, v: ak.pdarray):
 
 
 # magnitude/L_2-norm of arkouda pdarray
-def magnitude(u):
-    if isinstance(u, ak.pdarray):
-        return math.sqrt(dot(u, u))
-    else:
-        raise TypeError("u must be a pdarray")
+def magnitude(u: ak.pdarray):
+    return math.sqrt(dot(u, u))
 
 
 # cosine distance of two arkouda pdarrays
 # should function similarly to scipy.spatial.distance.cosine
-def cosine(u, v):
-    if isinstance(u, ak.pdarray) and isinstance(v, ak.pdarray):
-        return 1.0 - dot(u, v) / (magnitude(u) * magnitude(v))
-    else:
-        raise TypeError("u and v must be pdarrays")
+def cosine(u: ak.pdarray, v: ak.pdarray):
+    return 1.0 - dot(u, v) / (magnitude(u) * magnitude(v))
 
 
 # euclidean distance of two arkouda pdarrays
 # should function similarly to scipy.spatial.distance.euclidean
-def euclidean(u, v):
-    if isinstance(u, ak.pdarray) and isinstance(v, ak.pdarray):
-        return math.sqrt(dot(u, u) + dot(v, v) - 2*dot(u, v))
-    else:
-        raise TypeError("u and v must be pdarrays")
+def euclidean(u: ak.pdarray, v: ak.pdarray):
+    return math.sqrt(dot(u, u) + dot(v, v) - 2*dot(u, v))
