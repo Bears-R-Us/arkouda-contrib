b9b151fccd9a2665115bed9de550e7cd28ebe0c3
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-03-30 10:26:28 -0400

    Updated configuration to run pip install to properly path everything.

---
+++ arkouda_distance/client/README.md
@@ -0,0 +1,9 @@
+# Arkouda Distance
+
+Client only implementation of distance calculations.
+
+## Methods Implemented
+- `dot()` compute the dot product of 2 pdarrays.
+- `magnitude()` compute the magnitude/L2-Norm of a pdarray.
+- `consine()` compute the cosine distance of 2 pdarrays.
+- `euclidean()` compute the Euclidean distance of 2 pdarrays.
--- module_configuration.py
+++ module_configuration.py
@@ -5,22 +5,27 @@
 
 import os
 import optparse
+import pkg_resources as pr
+import subprocess
 
 
-def add_client_path(client_path):
+def install_client_pkg(client_path):
     """
-    Add path to the package to the PYTHONPATH environment variable.
-
-    :param client_path: absolute path to module location. Path should be 1 level above python package
+    Install the python package if not already installed.
+    :param client_path: absolute path to module
     """
-    if os.getenv('PYTHONPATH') is None:
-        # PYTHONPATH env var does not exist, add it and configure the module path
-        print(f'\nRun the Following Command:\n\texport PYTHONPATH={client_path}\n')
-    elif os.getenv('PYTHONPATH') is not None and client_path not in os.getenv('PYTHONPATH'):
-        # PYTHONPATH exists, but the module path is not added
-        print(f'\nRun the Following Command:\n\texport PYTHONPATH={client_path}:$PYTHONPATH\n')
-    else:
-        print('Environment Pathed Correctly')
+    # get list of installed pkgs
+    installed_pkgs = {pkg.key for pkg in pr.working_set}
+    if 'pip' not in installed_pkgs:
+        raise RuntimeError("pip is required for the client installation and is not installed.")
+    if not os.path.exists(client_path+"/setup.py"):
+        raise RuntimeError(f"Configuration requires a setup.py file to install client. Please configure {client_path}/setup.py")
+    p = subprocess.Popen(["python3", f"{client_path}/setup.py", "--name"])
+    pkg_name, error = p.communicate()
+
+    # Only provide install command if not already installed on system
+    if pkg_name not in installed_pkgs:
+        print(f"pip install {client_path}")
 
 
 def configure_server_module():
@@ -31,7 +36,7 @@ def run(mod_path):
     client_path = mod_path+"/client"
     server_path = mod_path+"/server"
     if os.path.exists(client_path):
-        add_client_path(client_path)
+        install_client_pkg(client_path)
         if os.path.isdir(mod_path+"/server"):
             configure_server_module()
     else:
