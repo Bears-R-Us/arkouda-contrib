6ae5eb19176e4c79c32abe2b42964e1cf3f66cc1
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-04-18 07:06:56 -0400

    Added @typechecked decorators

--- arkouda_distance/client/arkouda_distance/distance.py
+++ arkouda_distance/client/arkouda_distance/distance.py
@@ -4,25 +4,30 @@ from arkouda import pdarray
 from arkouda.pdarrayclass import sum
 
 import math
+from typeguard import typechecked
 
 
 # dot product or two arkouda pdarrays
+@typechecked
 def dot(u: pdarray, v: pdarray):
     return sum(u * v)
 
 
 # magnitude/L_2-norm of arkouda pdarray
+@typechecked
 def magnitude(u: pdarray):
     return math.sqrt(dot(u, u))
 
 
 # cosine distance of two arkouda pdarrays
 # should function similarly to scipy.spatial.distance.cosine
+@typechecked
 def cosine(u: pdarray, v: pdarray):
     return 1.0 - dot(u, v) / (magnitude(u) * magnitude(v))
 
 
 # euclidean distance of two arkouda pdarrays
 # should function similarly to scipy.spatial.distance.euclidean
+@typechecked
 def euclidean(u: pdarray, v: pdarray):
     return math.sqrt(dot(u, u) + dot(v, v) - 2*dot(u, v))
