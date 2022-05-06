c6c0c40b1772c9734b4536516e94eff98b34d4e7
mostroski <84462352+mostroski@users.noreply.github.com>
2022-05-06 12:02:30 -0400

    Adding exported aksparse source code (#16)
    * Adding exported aksparse source code
    
    * creating new test directory/file
    
    Co-authored-by: Michael Ostroski <michaelostroski@Michaels-MacBook-Pro.local>

---
+++ aksparse/test/aksparse_test.py
@@ -0,0 +1,10 @@
+from base_test import ArkoudaTest
+import arkouda as ak
+import aksparse as aks
+
+class aksparseTest(ArkoudaTest):
+
+    #TODO: write some small matrix test cases
+    #(also likely fix a bunch of broken code)
+    def test_aksparse(self):
+        self.assertEqual(1,1)
