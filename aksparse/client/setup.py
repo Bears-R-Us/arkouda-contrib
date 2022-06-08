98ac7b86002fc47bdf00295427b3d75d161aa8b5
Ethan-DeBandi99 <16845933+Ethan-DeBandi99@users.noreply.github.com>
2022-06-08 08:06:26 -0400

    Updated setup.py files to require python>=3.8 to align with arkouda updates to the same. Also ran black and isort on the files.

--- aksparse/client/setup.py
+++ aksparse/client/setup.py
@@ -1,27 +1,26 @@
-from setuptools import setup, find_packages
 from os import path
+
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as fh:
+with open(path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-        name='aksparse',
-        version='1.0.0',
-        author="U.S. Government",
-        author_email="",
-        description="Sparse matrix operations for arkouda",
-        long_description=long_description,
-        long_decription_content_type="text/markdown",
-        keywords="HPC exploratory analysis parallel distribute arrays Chapel sparse matrix linear",
-        packages=setuptools.find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3"
-        ],
-        install_requires=['numpy', 'pandas', 'scipy'],
-        python_requires='>= 3.6',
-        project_urls={
-            'Bug Reports': 'https://github.com/Bear-R-Us/arkouda/issues',
-        }
+setup(
+    name="aksparse",
+    version="1.0.0",
+    author="U.S. Government",
+    author_email="",
+    description="Sparse matrix operations for arkouda",
+    long_description=long_description,
+    long_decription_content_type="text/markdown",
+    keywords="HPC exploratory analysis parallel distribute arrays Chapel sparse matrix linear",
+    packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=["numpy", "pandas", "scipy"],
+    python_requires=">= 3.8",
+    project_urls={
+        "Bug Reports": "https://github.com/Bear-R-Us/arkouda/issues",
+    },
 )
--- arkouda_BlockArrays2D/client/setup.py
+++ arkouda_BlockArrays2D/client/setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup
 from os import path
+
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 # Long description will be contents of README
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='arkouda_BlockArrays2D',
-    version='0.0.2',
-    description='2 dimensional Arkouda pdarrays.',
+    name="arkouda_BlockArrays2D",
+    version="0.0.2",
+    description="2 dimensional Arkouda pdarrays.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=['arkouda_BlockArrays2D'],
-    python_requires=">=3.7",
-    install_requires=[
-        'arkouda'
-    ]
+    long_description_content_type="text/markdown",
+    packages=["arkouda_BlockArrays2D"],
+    python_requires=">=3.8",
+    install_requires=["arkouda"],
 )
