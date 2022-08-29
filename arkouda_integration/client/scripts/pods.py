17c8039f0ea2d3ce46e536eb8f8f909b601ff035
hokiegeek2 <hokiegeek2@gmail.com>
2022-08-29 19:20:48 +0000

    refactored to utilize env variables

--- arkouda_integration/client/scripts/pods.py
+++ arkouda_integration/client/scripts/pods.py
@@ -9,7 +9,7 @@ def main(crt_file : str, key_file : str, k8s_host : str, invocation_method : str
     dao = KubernetesDao(crt_file,key_file,k8s_host)
 
     pod_method = InvocationMethod(invocation_method)
-    print(pod_method)
+
     if pod_method == InvocationMethod.GET_POD_IPS:
         print(
             dao.get_pod_ips(namespace=namespace, 
@@ -28,60 +28,13 @@ def main(crt_file : str, key_file : str, k8s_host : str, invocation_method : str
         )
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser(description="Arkouda Kubernetes pod script")
-
-    required = arg_parser.add_argument_group("required arguments")
-    optional = arg_parser.add_argument_group("optional arguments")
-
-    required.add_argument(
-        "-c", "--crt_file", 
-        type=str, 
-        required=True,
-        help="TLS crt file for connecting to Kubernetes")
-    
-    required.add_argument(
-        "-k", "--key_file", 
-        type=str, 
-        required=True,
-        help="TLS key file for connecting to Kubernetes")
-    
-    required.add_argument(
-        "-kh", "--k8s_host",
-        type=str,
-        required=True,
-        help="Kubernetes API URL (from kubectl cluster-info command)")
-    
-    required.add_argument(
-        "-i",
-        "--invocation_method",
-        type=str,
-        help="the KubernetesDao method to invoke",
-        required=True,
-    )
-
-    required.add_argument(
-        "-a", "--app_name", 
-        type=str, 
-        required=True,
-        help="Kubernetes app name"
-    )
-    optional.add_argument(
-        "-n",
-        "--namespace",
-        type=str,
-        help="Kubernetes namespace, defaults to default",
-        default="default",
-        required=False,
-    )
-
-    args = arg_parser.parse_args()
-    
-    crt_file = args.crt_file
-    key_file = args.key_file
-    k8s_host = args.k8s_host
-    invocation_method = InvocationMethod(args.invocation_method)
-    app_name = args.app_name
-    namespace = args.namespace
+    import os
+    crt_file = os.environ['CERT_FILE']
+    key_file = os.environ['KEY_FILE']
+    k8s_host = os.environ['K8S_HOST']
+    invocation_method = os.environ['POD_METHOD']
+    app_name = os.environ['APP_NAME']
+    namespace = os.environ['NAMESPACE']
 
     main(crt_file=crt_file,
          key_file=key_file,
