a4b13bb28b1c458d40b3ebba47d1b675db36edbc
compiling-is-winning <92549922+compiling-is-winning@users.noreply.github.com>
2022-10-19 15:15:36 -0400

    Initial serial version

--- arkouda_cws_hashing/test/lsh_minmax.py
+++ arkouda_cws_hashing/test/lsh_minmax.py
@@ -2,7 +2,7 @@ from base_test import ArkoudaTest
 import arkouda as ak
 import lsh_minmax as lsh
 
-class DistanceTest(Arkouda
+class LSHMinMaxTest(ArkoudaTest):
     def test_lsh(self):
         a = ak.array([0, 3, 10, 15, 27, 31, 38, 40, 44, 49, 50])
         b = ak.array([3, 4, 1, 3, 7, 8, 2, 1, 8, 4, 2, 5, 6, 9, 2, 5, 3, 2, 8, 8, 9, 1, 5, 3, 9, 3, 3, 3, 1, 8, 1, 6, 9, 9, 8, 7, 3, 5, 1, 1, 3, 8, 8, 6, 4, 6, 5, 1, 7, 1])
