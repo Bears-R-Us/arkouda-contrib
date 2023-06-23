359faa1c12076ea469474830b906a1ea740f6077
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2023-06-23 12:40:04 -0400

    updates per integration testing #98

--- arkouda_proxy_client/arkouda_proxy_client/client.py
+++ arkouda_proxy_client/arkouda_proxy_client/client.py
@@ -15,6 +15,13 @@ logger = getArkoudaLogger(name="Arkouda Client", logLevel=LogLevel.DEBUG)
 
 class GrpcChannel(Channel):
     
+    __slots__ = ('arkouda_url')
+
+    def __init__(self, user: str, server: str='localhost', port: int=5555, token: str=None, 
+                 connect_url: str=None, arkouda_url: str=None) -> None:
+        Channel.__init__(self, user, server, port, token=token, connect_url=connect_url)
+        self.arkouda_url = arkouda_url
+
     def send_string_message(self, cmd: str, recv_binary: bool = False, args: str = None, 
                             size: int = -1) -> Union[str, memoryview]:
         logger.debug("Sending request to Arkouda gRPC ...")
@@ -53,7 +60,7 @@ class GrpcChannel(Channel):
             return raw_response
         except Exception:
             response = ArkoudaResponse()
-            response.message = '{"msg": "Arkouda is unavailable"}'
+            response.message = json.dumps({"msg": f"Arkouda at the address {self.url} is unavailable"})
             return response
 
     def send_binary_message(self, cmd: str, payload: memoryview, recv_binary: bool=False, 
@@ -70,6 +77,12 @@ class GrpcChannel(Channel):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test arkouda_proxy_server')
 
+    parser.add_argument('--arkouda_url', type=str,
+                        help='the arkouda url in the tcp://host:port format')
+    parser.add_argument('--arkouda_host', type=str,
+                        help='the arkouda host machine ip address, host, or service name')
+    parser.add_argument('--arkouda_port', type=int,
+                        help='the arkouda port')
     parser.add_argument('--arkouda_proxy_url', type=str, required=True,
                         help='the arkouda_proxy_server url in the host:port format')
     parser.add_argument('--user', type=str, required=True,
@@ -86,6 +99,14 @@ if __name__ == '__main__':
                         help='space-delimited list of args for the cmd')
 
     args = parser.parse_args()
+    
+    def generate_arkouda_url(host: str='localhost', port: int=5555) -> str:
+        return f'tcp://{host};{port}]'
+        
+    
+    arkouda_url = args.arkouda_url if args.arkouda_url else generate_arkouda_url(args.arkouda_host, 
+                                                                                 args.arkouda_port)
+            
 
-    channel = GrpcChannel(server='localhost', port=5555, connect_url=args.arkouda_proxy_url, user=args.user)
+    channel = GrpcChannel(arkouda_url=args.arkouda_url, connect_url=args.arkouda_proxy_url, user=args.user)
     channel.send_string_message(args.cmd, recv_binary=False, args=args.args, size=args.size)
