36f09367cdb71ec2e3b164d19d53636a68db9222
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2023-06-27 18:03:43 -0400

    updated per PR feedback #98

--- arkouda_proxy_client/setup.py
+++ arkouda_proxy_client/setup.py
@@ -112,7 +112,7 @@ setup(
     # project page. What does your project relate to?
     #
     # Note that this is a string of words separated by whitespace, not a list.
-    keywords='HPC workflow exploratory analysis parallel distribute arrays Chapel',  # Optional
+    keywords='HPC workflow exploratory analysis parallel distribute arrays rust gRPC',  # Optional
 
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
