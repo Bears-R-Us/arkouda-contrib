1a9c6bb578d865c72f0599cbc0f5f7686c487051
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2023-09-06 16:16:44 -0400

    removed commented-out code #104

--- arkouda_streaming_proxy_server/src/server.rs
+++ arkouda_streaming_proxy_server/src/server.rs
@@ -3,12 +3,16 @@ pub mod streaming_arkouda {
 }
 
 use std::env;
+use std::future::{Future, Pending};
+use std::task::{Poll, Ready};
 use log::{info,debug};
 use tokio_stream::StreamExt;
 use tonic::{transport::Server, Request, Response, Status, Streaming};
 
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+
+use zmq::{Socket, Context};
 
 use streaming_arkouda::streaming_arkouda_server::{StreamingArkouda, StreamingArkoudaServer};
 use streaming_arkouda::{ArkoudaReply, ArkoudaRequest};
@@ -24,6 +28,36 @@ struct ArkoudaMessage {
 }
 
 
+pub struct ZmqRead<'a> {
+    socket: &'a Socket,
+}
+
+trait ZmqFuture {
+    type Output;
+    result: String;
+    fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+}
+
+impl ZmqFuture for ZmqRead<'_> {
+    type Output = Vec<u8>;
+
+    fn poll(&mut self, wake: fn()) -> Poll<Self::Output> {
+        if self.socket.rcv_buff() {
+            // The socket has data -- read it into a buffer and return it.
+            Poll::Ready(self.socket.recv_msg())
+        } else {
+            // The socket does not yet have data.
+            //
+            // Arrange for `wake` to be called once data is available.
+            // When data becomes available, `wake` will be called, and the
+            // user of this `Future` will know to call `poll` again and
+            // receive data.
+            self.socket.set_readable_callback(wake);
+            Poll::Pending
+        }
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct StreamingArkServer {
     url: String
@@ -34,10 +68,10 @@ pub struct StreamingArkServer {
 impl StreamingArkouda for StreamingArkServer {
 
     async fn handle_request_stream(&self, request: Request<Streaming<ArkoudaRequest>>) -> Result<Response<ArkoudaReply>, Status> {
-        debug!("polling message from Arkouda");
+        debug!("received message from Arkouda");
         let mut stream = request.into_inner();
 
-        let ctx = zmq::Context::new();
+        let ctx = Context::new();
         let socket = ctx.socket(zmq::REQ).unwrap();
 
         socket.connect(&self.url).unwrap();
@@ -67,14 +101,9 @@ impl StreamingArkouda for StreamingArkServer {
 
             debug!("Return message from Arkouda: {}", result);
 
-            //let reply = streaming_arkouda::ArkoudaReply {
-            //    message: format!("{}", result).into(),
-            //};
             reply.message = format!("{}", result).into();
-
-            //Ok(Response::new(reply))
         }
-        
+        debug!("Result from Arkouda: {}", reply.message);
         Ok(Response::new(reply))
     }
 }
