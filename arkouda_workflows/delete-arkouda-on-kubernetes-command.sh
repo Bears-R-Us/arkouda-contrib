35d4c1401cd1f3fbc987fc08a3bb9bc2550c7d97
hokiegeek2 <hokiegeek2@users.noreply.github.com>
2023-09-16 11:11:26 -0400

    incremental checkin #118

--- arkouda_workflows/delete-arkouda-on-kubernetes-command.sh
+++ arkouda_workflows/delete-arkouda-on-kubernetes-command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 argo submit -n arkouda delete-arkouda-on-kubernetes-workflow.yaml -p arkouda-ssl-secret=arkouda-tls \
-            -p arkouda-instance-name=arkouda-on-k8s -p kubernetes-api-url=https://192.168.1.11:6443 \
+            -p arkouda-instance-name=arkouda-on-k8s -p kubernetes-api-url=$KUBERNETES_URL \
             -p arkouda-user=arkouda 
