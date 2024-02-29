d77b6be2550a7c0f79163dd7863b2fbaa2a4e702
hokiegeek2 <hokiegeek2@gmail.com>
2024-02-29 14:06:29 +0000

    added support for ServiceMonitor #152

--- arkouda_workflows/deploy-prometheus-arkouda-exporter-command.sh
+++ arkouda_workflows/deploy-prometheus-arkouda-exporter-command.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-argo submit -n $ARKOUDA_EXPORTER_NAMESPACE \
+argo submit -n $ARKOUDA_NAMESPACE \
             deploy-prometheus-arkouda-exporter-workflow.yaml \
-            -p exporter-release-version=$ARKOUDA_EXPORTER_VERSION \
+            -p exporter-release-version=$ARKOUDA_VERSION \
             -p exporter-service-name=$ARKOUDA_EXPORTER_SERVICE_NAME \
             -p exporter-app-name=$ARKOUDA_EXPORTER_APP_NAME \
             -p arkouda-metrics-service-host=$ARKOUDA_METRICS_SERVICE_HOST \
             -p arkouda-metrics-service-port=$ARKOUDA_METRICS_SERVICE_PORT \
             -p arkouda-server-name=$ARKOUDA_SERVER_NAME \
-            -p exporter-polling-interval=15
+	    -p arkouda-launcher=$ARKOUDA_LAUNCHER \
+            -p exporter-polling-interval=15 \
+	    -p prometheus-match-label="$ARKOUDA_PROMETHEUS_MATCH_LABEL"
