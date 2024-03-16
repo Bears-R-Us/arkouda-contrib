62d4c8edb24cc6f4e31b75506c580567a2b5ea17
hokiegeek2 <hokiegeek2@gmail.com>
2024-03-16 16:17:33 +0000

    added ability run Arkouda-on-Kubernetes as the default user (ubuntu) or configured user #154

--- arkouda_workflows/delete-arkouda-on-kubernetes-command.sh
+++ arkouda_workflows/delete-arkouda-on-kubernetes-command.sh
@@ -2,7 +2,5 @@
 
 argo submit -n $ARKOUDA_NAMESPACE \
             delete-arkouda-on-kubernetes-workflow.yaml \
-            -p arkouda-ssl-secret=$ARKOUDA_SSL_SECRET \
             -p arkouda-instance-name=$ARKOUDA_INSTANCE_NAME \
             -p kubernetes-api-url=$KUBERNETES_URL \
-            -p arkouda-user=$ARKOUDA_USER
